6bb58e02b653c391ef6ee4c33131dd0f12b15af6
朱庭宏 <did1335@gmail.com>
2020-04-26 00:54:05 +0800

    Initial commit

---
+++ src/js/view/app.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    data: {
+        community: 'Wikidata 地方學專題聚會',
+        newtaipei: '新北主持人: 劉健豐、張以璇',
+        taichung: '台中主持人: 朱庭宏',
+        join: '參與計畫',
+        fb: 'https://www.facebook.com/WikiLocalogy/?modal=admin_todo_tour'
+    }
+})
