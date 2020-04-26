63d0cd1e84f0d143704017e6d37695d479832ab9
朱庭宏 <did1335@gmail.com>
2020-04-26 17:39:47 +0800

    noscript

--- src/js/view/app.js
+++ src/js/view/app.js
@@ -1,11 +1,21 @@
 var app = new Vue({
     data: {
+        title: '開源<span style="vertical-align: top;">x</span>創生',
+        slogan1: '當<span>開源</span>與<span>文史</span>相遇時',
+        slogan2: '<span>創造</span>歷史建築的<span>新生命</span>',
+        finger: 'src/svg/scroll.svg',
         wikidata: 'src/img/wikidata.PNG',
+        wikidataStyle: 'cursor: pointer;',
         intro: 'Wikidata 地方學專題聚會，是第一個 Wikidata Taiwan 社群的主題性編輯聚會。在 2019年末由幾位對於地方學有研究興趣的參與者與社群共同討論後，發現地方學資料以結構化方式呈現的比例非常稀少；使得臺灣雖然有大量的地方學資料釋出，但是在結構化領域上卻依然從缺，因此在經過籌備後決定以地方志為主要參考書目，搭配實體走讀等活動所產出的數位媒體檔案，將地方學資料轉進 Wikidata 與相關維基姊妹計畫。透過這樣的方式擴展地方學研究的可能性，也能透過結構化的方式創造資料應用的新想像。',
         community: 'Wikidata 地方學專題聚會',
         newtaipei: '新北主持人: 劉健豐、張以璇',
         taichung: '台中主持人: 朱庭宏',
         join: '參與計畫',
         fb: 'https://www.facebook.com/WikiLocalogy/?modal=admin_todo_tour'
-    }
+    },
+    methods: {
+        goWikidata: function() {
+            window.open('https://www.wikidata.org/wiki/Wikidata:Main_Page');
+        }
+    },
 })
